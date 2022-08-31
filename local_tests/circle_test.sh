0e728190fb56e107fa9b511ba5286f6ac3c7ee21
david-gary <dgary416@gmail.com>
2022-08-30 23:26:56 -0400

    renaming fixes it???

--- local_tests/circle_test.sh
+++
@@ -1,4 +0,0 @@
-# Test Case 2: Circle Class Test
-echo "Test Case 2: Circle Class Test"
-echo "============================"
-python3 ../circle.py < inputs/circle_test_input.txt > outputs/circle_test_output.txt
--- local_tests/person_test.sh
+++
@@ -1,4 +0,0 @@
-# Test Case 1: Person Class Test
-echo "Test Case 1: Person Class Test"
-echo "============================"
-python3 ../person.py > outputs/person_test_out.txt
