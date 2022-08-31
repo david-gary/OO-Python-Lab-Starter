2856b8cda6c01e6159f617f891beb27fd1643266
david-gary <dgary416@gmail.com>
2022-08-30 23:32:28 -0400

    or not

---
+++ LocalTests/circle_test.sh
@@ -0,0 +1,4 @@
+# Test Case 2: Circle Class Test
+echo "Test Case 2: Circle Class Test"
+echo "============================"
+python3 ../circle.py < inputs/circle_test_input.txt > outputs/circle_test_output.txt
---
+++ LocalTests/person_test.sh
@@ -0,0 +1,4 @@
+# Test Case 1: Person Class Test
+echo "Test Case 1: Person Class Test"
+echo "============================"
+python3 ../person.py > outputs/person_test_out.txt
